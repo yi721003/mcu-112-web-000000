da81d76034c7bdd88780a420e501394ae09595b7
Oliver <yi721003@gmail.com>
2023-11-17 15:22:24 +0800

    feat(todo): 利用事件繫結設定待辦事項狀態

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -10,4 +10,10 @@ import { CommonModule } from '@angular/common';
 })
 export class TodoComponent {
   content = '待辦事項 A';
+
+  hasFinished = false;
+
+  onSetStatus(hasFinished: boolean): void {
+    this.hasFinished = hasFinished;
+  }
 }
